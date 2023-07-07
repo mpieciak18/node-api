b3361f3fcad6d8a4479405b0debbf4b6fc84ed88
Mark Pieciak <mpieciak18@msn.com>
2023-07-07 16:35:30 -0400

    add input validation middleware to routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -8,37 +9,79 @@ router.get('/product', (req, res) => {
 	res.json({ message: 'message' });
 });
 router.get('/product/:id', () => {});
-router.put('/product/:id', body('name').isString(), (req, res) => {
-	const errors = validationResult(req);
-	console.log(errors);
-
-	if (!errors.isEmpty()) {
-		res.status(400);
-		res.json({ errors: errors.array() });
-	}
-});
-router.post('/product', () => {});
+router.put(
+	'/product/:id',
+	body('name').isString(),
+	handleInputErrors,
+	() => {}
+);
+router.post(
+	'/product',
+	body(['name', 'belongsToId']).isString(),
+	handleInputErrors,
+	() => {}
+);
 router.delete('/product/:id', () => {});
 
 // Updates
 router.get('/update', () => {});
 router.get('/update/:id', () => {});
-router.put('/update/:id', () => {});
-router.post('/update', () => {});
+router.put(
+	'/update/:id',
+	body('updatedAt').isDate(),
+	body(['title', 'body']).isString(),
+	body('updatePoints').isArray(),
+	handleInputErrors,
+	() => {}
+);
+router.post(
+	'/update',
+	body('updatedAt').isDate(),
+	body(['title', 'body', 'productId']).isString(),
+	body('updatePoints').isArray(),
+	handleInputErrors,
+	() => {}
+);
 router.delete('/update/:id', () => {});
 
 // Update Points
 router.get('/updatepoint', () => {});
 router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
+router.put(
+	'/updatepoint/:id',
+	body('updatedAt').isDate(),
+	body(['name', 'description', 'updateId']).isString(),
+	body('products').isArray(),
+	handleInputErrors,
+	() => {}
+);
+router.post(
+	'/updatepoint',
+	body('updatedAt').isDate(),
+	body(['name', 'description', 'updateId']).isString(),
+	body('products').isArray(),
+	handleInputErrors,
+	() => {}
+);
 router.delete('/updatepoint/:id', () => {});
 
 // Update Points
-router.get('/updatepoint', () => {});
-router.get('/updatepoint/:id', () => {});
-router.put('/updatepoint/:id', () => {});
-router.post('/updatepoint', () => {});
-router.delete('/updatepoint/:id', () => {});
+router.get('/user', () => {});
+router.get('/user/:id', () => {});
+router.put(
+	'/user/:id',
+	body(['username', 'password']).isString(),
+	body('products').isArray(),
+	handleInputErrors,
+	() => {}
+);
+router.post(
+	'/user',
+	body(['username', 'password']).isString(),
+	body('products').isArray(),
+	handleInputErrors,
+	() => {}
+);
+router.delete('/user/:id', () => {});
 
 export default router;
