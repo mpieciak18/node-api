923f3c50179cdf442a7cd318a457282bc36f10c1
Mark Pieciak <mpieciak18@msn.com>
2023-07-09 19:02:19 -0400

    add process err handling at top level

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,11 @@ import app from './server';
 app.listen(3001, () => {
 	console.log('hello on http://localhost:3001');
 });
+
+process.on('uncaughtException', () => {
+	console.log('uncaught exception (sync) at top level of node process');
+});
+
+process.on('unhandledRejection', () => {
+	console.log('unhandled rejection (async) at top level of node process');
+});
